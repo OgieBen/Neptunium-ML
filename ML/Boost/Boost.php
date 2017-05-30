44ad64ec66ae9316f093225e8dccadb38b5daba5
Ogie Ben <bensoft2k5@gmail.com>
2017-05-30 21:05:30 +0100

    Update Boost.php

--- ML/Boost/Boost.php
+++ ML/Boost/Boost.php
@@ -2,8 +2,6 @@
 
 namespace Boost;
 
-//unstable
-
 use NetworkRelations;
 use DataProcessor\DataProcessor;
 
@@ -13,38 +11,36 @@ class Boost {
         ;
     }
 
-    public static function dStump($sample)
-    {
-      //  $sat = array();
+    public static function dStump($sample) {
+        //  $sat = array();
         $predictedLabels = array();
-        
 
-        /* $sample must be 3-d matrix or array
+
+        /* $sample must be 3-d matrix or array.
          * an array that holds other 2-d array(s)
          * 
          * TODO: validate matrix or array
          */
-       
-       
-        list($avgSAT, $sat) = self::avgSAT($sample,true);
-        for($run =0; $run<count($sample);$run++)
-        {
-           // array_push($sat,$val=DataProcessor::integralImage($sample[$run]));
-          //  echo"!--------!";
-           // var_dump( array_sum($sat[$run]));
-           // echo"!--------!";
-          
-           
-            if((self::Sum2d($sat[$run]) - $avgSAT) > ($avgSAT/4))
-            {
-            array_push($predictedLabels,0);
-            continue;
+
+
+        list($avgSAT, $sat) = self::avgSAT($sample, true);
+        for ($run = 0; $run < count($sample); $run++) {
+            // array_push($sat,$val=DataProcessor::integralImage($sample[$run]));
+            //  echo"!--------!";
+            // var_dump( array_sum($sat[$run]));
+            // echo"!--------!";
+
+
+            if ((self::Sum2d($sat[$run]) - $avgSAT) > ($avgSAT / 4)) {
+                array_push($predictedLabels, 0);
+                continue;
             }
-            
-             array_push($predictedLabels,1);
+
+            array_push($predictedLabels, 1);
         }
-        
-        return $predictedLabels;         
+
+
+        return $predictedLabels;
     }
 
     /* use float -> floatBoost
@@ -52,117 +48,122 @@ class Boost {
      */
 
     public static function train(
-    $trainer = 'ada', $weakL = 'dStump', $sample, $labels, $runs = 100) {
+    $trainer = 'ada', $weakL = ['dStump'], $sample, $labels) {
         $probArray = [];
+        $sampleSize = count($sample);
         for ($i = 0; $i < $sampleSize; $i++) {
             array_push($probArray, (1 / $sampleSize));
         }
-
-        self::adaBoost($runs, $weakL, $sample, $labels, $probArray);
+        
+        $boost = strtolower($trainer) . 'Boost';
+        return self::$boost($weakL, $sample, $labels, $probArray);
     }
 
-    private static function adaBoost(
-    $runs, $weakL, $sample, $labels, $probArray) {
+    private static function adaBoost($weakL, $sample, $labels, $probArray) {
 
         $error = [];
         $weight = [];
         $tracker = 0;
+        $temp = array();
         $probArrayM = $probArray;
         $sampleSize = count($sample);
         // $probTempArray = [$probArrayM];
         $hypothesis = [];
-
+        $wyhTop=0;
+        $wyhBottom = 0;
         $eTemp = [];
 
-        for ($i = 0; $i < $runs; $i++) {
+        for ($i = 0; $i < count($weakL); $i++) {
 
             //use weakL to check and call weak learners
-            $wkL = self::$weakL($probArrayM, $sample);
+ 
+            $learner = $weakL[$i]; 
+            $wkL = self::$learner($sample);
+           array_push($hypothesis, $wkL);
 
             //run through the results of the decision stump 
-            for ($trck = 0; $trck < $wkL; $trck) {
+            for ($trck = 0; $trck < count($wkL); $trck++) {
                 //check for misclassification
-                if ($wkL[$trck] != $labels[$trck]) {
-                    //send in error values
-                    array_push($eTemp, $probArrayM[$trck]);
+                $temp[$trck] = ($wkL[$trck] != $labels[$trck] );
+                if ($temp[$trck]) {
+                    var_dump($wkL[$trck]);
+                    //send in weight values of the misclassified samples
+                    $eTemp[$trck] = $probArrayM[$trck];
+                    // array_push( $weight, (1 / 2 * (log((1 / $error[$trck]) - 1))));
                 }
             }
 
-            array_push($error, array_sum($eTemp));
+           
+
+            array_push($error, $e = array_sum($eTemp));
 
             //calculate weight
-           array_push( $weight, (1 / 2 * (log((1 / $error[$i]) - 1))));
-
-
-
-            $wyhTop = exp(-($weight[$i] * $weakL[$tracker] * $sample[$tracker]));
-
+            array_push($weight, $w = (0.5 * (log((1 / $e) - 1))));
 
             for ($j = 0; $j < $sampleSize; $j++) {
-                $wyhBottom += ($probArrayM[$j]) * (exp(-($weight[$i] * $weakL[$j] * $sample[$j])));
-            }
-
-             //update probability
-            $probArrayM[$i] = ($probArrayM[$tracker] * $wyhTop) / ($wyhBottom);
-
-            
-            if ($tracker == ($sampleSize - 1)) {
-                $tracker = 0;
+                
+                $wyhBottom += ($probArrayM[$j]) * (exp(-($w * $wkL[$j] * $labels[$j])));
+                var_dump($wyhBottom);
+                
             }
             
+            $f = 0;
+            foreach ($temp as $predLabel) {
+                if($predLabel){
+                $wyhTop = $probArrayM[$f] * exp(-($w * $wkL[$f] * $labels[$f]));
+                $probArrayM[$f] = ($wyhTop/$wyhBottom);         
+                }
+                $f++;
+            }
+
+           
+            
+            $eTemp = array();
         }
 
-        //store hypothesis
-            array_push($hypothesis, NetworkRelations\NetworkRelations::hardLimit(
-                        //may not work
-                            function ()
-                            {
-                                $val = 0;
-                                for($m=0;$m<$runs;$m++){
-                                   $val += ($weight[$m] * $wkL[$m]);
-                                    }
-
-                                return $val;
-                            }
-                           )
-                        
-                        );
-            
-
-        return $hypothesis;
+          return [$weight,$hypothesis]; 
     }
+
+   public static function evaluateStrgHyp($weight,$hypothesis,$sampleId)
+   {
+       $pred = 0;
+      
+       for($i =0;$i<count($weight);$i++)
+       {
+         
+         $pred += $weight[$i]* $hypothesis[$i][$sampleId]; 
+        
+       }
+       return $pred;
+   }    
+    
     
     /*
      * computes the average of Summed Area Table(SAT)
      */
-    public static function avgSAT($sample,$bool = false)
-    {
-         $sat = array();
-         $sum = 0;
-        for($run =0; $run< $total=count($sample);$run++)
-        {
-            array_push($sat,$val=DataProcessor::integralImage($sample[$run]));
+
+    public static function avgSAT($sample, $bool = false) {
+        $sat = array();
+        $sum = 0;
+        for ($run = 0; $run < $total = count($sample); $run++) {
+            array_push($sat, $val = DataProcessor::integralImage($sample[$run]));
             $sum += self::Sum2d($val);
         }
-     
-       $avg=($sum/$total);
-       if($bool)
-       {
-           return  [$avg,$sat] ;
-       }
-            return $avg;   
+
+        $avg = ($sum / $total);
+        if ($bool) {
+            return [$avg, $sat];
+        }
+        return $avg;
     }
-    
-    public static function Sum2d($array2d)
-    {
+
+    public static function Sum2d($array2d) {
         $val = 0;
-     foreach($array2d as $ar)
-     {
-         $val += array_sum($ar);
-     }
-     return $val;
+        foreach ($array2d as $ar) {
+            $val += array_sum($ar);
+        }
+        return $val;
     }
-    
 
     private function floatBoost() {
         
